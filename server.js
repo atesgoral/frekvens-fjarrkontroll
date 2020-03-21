0c63d504bf82d13ea8028e3accc374ba9f9b050e
Glitch (hello-express) <none>
2020-03-21 15:10:04 +0000

    🍏✍️ Checkpoint ./package.json:13180552/1285 ./server.js:13180552/1722 ./views/index.html:13180552/896

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
-// server.js
-// where your node app starts
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+http.createServer(app);
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+const io = socketIo(http);
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+
+
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+http.listen(process.env.PORT, () => {
+  console.log('Listening on', process.env.PORT);
 });
