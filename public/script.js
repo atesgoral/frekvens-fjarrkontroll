54e36a21c8b8f6ae2aa5583c59f47d168f97b589
Glitch (hello-express) <none>
2020-03-21 16:42:47 +0000

    🎏🌜 Checkpoint ./public/script.js:13180552/6

--- public/script.js
+++ public/script.js
@@ -2,9 +2,11 @@ function init(socket) {
   socket.on('connect', () => {
     console.log('Connected');
   });
+  
   socket.on('event', (data) => {
     console.log('Data', data);
   });
+  
   socket.on('disconnect', () => {
     console.log('Disconnected');
   });
